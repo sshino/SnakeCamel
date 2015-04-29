a4edac0cec598cece991acbc6e1d696efbf007ec
g12945ss <g12945ss.git@tsuda.ac.jp>
2015-04-29 12:03:12 +0900

    テストケースの変更①

--- snake-camel/test/snakecamel/SnakeCamelUtillTest.java
+++ snake-camel/test/snakecamel/SnakeCamelUtillTest.java
@@ -1,14 +1,24 @@
 package snakecamel;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 
 public class SnakeCamelUtillTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeToCamelcaseでスネークからキャメルに() {
+		SnakeCamelUtil snaketocamel = new SnakeCamelUtil();
+		String expected = "theBook";
+		String actual = snaketocamel.snakeToCamelcase("the_book");
+		assertThat(actual, is(expected));
 	}
-
+	@Test
+	public void camelToSnakecaseでキャメルからスネークに(){
+		SnakeCamelUtil cameltosnake = new SnakeCamelUtil();
+		String expected = "the_book";
+		String actual = cameltosnake.camelToSnakecase("theBook");
+		assertThat(actual, is(expected));
+	}
 }
